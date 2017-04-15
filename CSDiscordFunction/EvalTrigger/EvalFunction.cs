638822c8df32406d3aa2645962cf31c7d3baf9c5
Chris Curwick <ccurwick@exoronet.net>
2017-04-14 18:23:29 -0700

    Move to a compiled library and .net 4.6.2 !

---
+++ CSDiscordFunction/EvalTrigger/EvalFunction.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Scripting;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.Azure.WebJobs.Host;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using System.Web.Http;
+namespace CSDiscordFunction
+{
+    public class EvalFunction
+    {
+
+        private static readonly string[] DefaultImports =
+        {
+            "System",
+            "System.IO",
+            "System.Linq",
+            "System.Collections.Generic",
+            "System.Text",
+            "System.Text.RegularExpressions",
+            "System.Net",
+            "System.Threading",
+            "System.Threading.Tasks",
+            "System.Net.Http"
+        };
+
+        private static readonly Assembly[] DefaultReferences =
+        {
+            typeof(Enumerable).Assembly,
+            typeof(List<string>).Assembly,
+            typeof(JsonConvert).Assembly,
+            typeof(HttpConfiguration).Assembly,
+            typeof(string).Assembly
+        };
+
+        private static readonly string[] BlockedTokens =
+        {
+            ".GetEnvironmentVariable",
+        };
+
+
+        public static async Task<HttpResponseMessage> Run(HttpRequestMessage req, TraceWriter log)
+        {
+            var code = await req.Content.ReadAsStringAsync();
+            if (BlockedTokens.Any(a => code.Contains(a)))
+            {
+                log.Error("Forbidden token in request");
+                return req.CreateResponse(HttpStatusCode.Forbidden, "Class not allowed");
+            }
+
+            ScriptState<object> result = null;
+
+            Exception evalException = null;
+            Stopwatch sw = null;
+
+            try
+            {
+                sw = Stopwatch.StartNew();
+                result = await new Runner().Run(code);
+                sw.Stop();
+            }
+            catch (Exception ex)
+            {
+                evalException = ex;
+                log.Error(ex.ToString());
+            }
+            finally
+            {
+                if (sw?.IsRunning ?? false)
+                {
+                    sw?.Stop();
+                }
+            }
+
+            if (result != null && result.Exception == null)
+            {
+                log.Info($"executed '{code}'");
+                return req.CreateResponse(HttpStatusCode.OK, new Result(result, sw.Elapsed, evalException));
+            }
+            else
+            {
+                log.Warning($"failed to execute '{code}'");
+                return req.CreateResponse(HttpStatusCode.BadRequest, new Result(result, sw.Elapsed, evalException));
+            }
+        }
+
+        public class Runner
+        {
+            public async Task<ScriptState<object>> Run(string code)
+            {
+                var options = ScriptOptions.Default
+                      .WithImports(DefaultImports)
+                      .WithReferences(DefaultReferences);
+
+                return await CSharpScript.RunAsync(code, options);
+            }
+        }
+
+        public class Result
+        {
+            public Result(ScriptState<object> state, TimeSpan executionTime, Exception ex = null)
+            {
+                if (state == null && ex == null)
+                {
+                    throw new ArgumentNullException(nameof(state));
+                }
+
+                ExecutionTime = executionTime;
+
+                if (state == null)
+                {
+                    Exception = ex.Message;
+                    ExceptionType = ex.GetType().Name;
+                    return;
+                }
+
+                ReturnValue = state.ReturnValue;
+                Code = state.Script.Code;
+                Exception = state.Exception?.Message ?? ex?.Message;
+            }
+
+            public object ReturnValue { get; set; }
+
+            public string Exception { get; set; }
+
+            public string ExceptionType { get; set; }
+
+            public string Code { get; set; }
+
+            public TimeSpan ExecutionTime { get; set; }
+        }
+    }
+}
---
+++ CSDiscordFunction/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CSDiscordFunction")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CSDiscordFunction")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d338c62-22e5-4bfe-8fab-c5371257d9cf")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
