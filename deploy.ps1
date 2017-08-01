7c9bd247bbbda49502eedf9398d932c6f42b5fcf
Chris Curwick <ccurwick@exoronet.net>
2017-07-31 21:12:48 -0700

    Appveyor agent deploy script

--- deploy.ps1
+++ deploy.ps1
@@ -1,19 +1,10 @@
 $ErrorActionPreference = 'Stop';
 
-#if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
-#  Write-Host "No version tag detected. Skip publishing."
-#  exit 0
-#}
 $tag = $env:APPVEYOR_REPO_TAG_NAME
-
 if([System.String]::IsNullOrWhitespace($tag)) {
     $tag = "untagged"
 }
 
-$env | out-host
-Write-Host Starting deploy
-docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
-
-docker tag csdiscordservice cisien/csdiscord:$tag
-
-docker push cisien/csdiscord:$tag
+docker stop CSDiscord
+docker rm CSDiscord
+docker run -d -e "tokens=$env:TOKENS" -e "APPINSIGHTS_INSTRUMENTATIONKEY=e210bb4c-ee0f-4d1f-a892-9b9728fec526" -p 80:80/tcp --name CSDiscord "cisien/csdiscord:$tag"
