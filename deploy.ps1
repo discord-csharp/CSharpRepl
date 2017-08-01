272b664cf8540c4e6ab472e59fb23f41186a7f93
Chris <Cisien@users.noreply.github.com>
2017-07-31 17:38:44 -0700

    Create deploy.ps1

---
+++ deploy.ps1
@@ -0,0 +1,19 @@
+$ErrorActionPreference = 'Stop';
+
+#if (! (Test-Path Env:\APPVEYOR_REPO_TAG_NAME)) {
+#  Write-Host "No version tag detected. Skip publishing."
+#  exit 0
+#}
+$tag = $env:APPVEYOR_REPO_TAG_NAME
+
+if([System.String]::IsNullOrWhitespace($tag)) {
+    $tag = "untagged"
+}
+
+$env | out-host
+Write-Host Starting deploy
+docker login -u="$env:DOCKER_USER" -p="$env:DOCKER_PASS"
+
+docker tag onibot cisien/csdiscord:$tag
+
+docker push cisien/csdiscord:$tag
