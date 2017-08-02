0e08bcd4bdb7ef4b6c55b77708b2ecefaade425d
Chris Curwick <ccurwick@exoronet.net>
2017-08-01 18:55:17 -0700

    set a net-alias and explicitly set the network

--- before-deploy.ps1
+++ before-deploy.ps1
@@ -13,5 +13,5 @@ if (Enter-OncePerDeployment "install_docker_image")
 	docker stop CSDiscord
 	docker rm CSDiscord
 	docker pull "cisien/csdiscord:$tag"
-	docker run -d -e "tokens=$env:TOKENS" -e "APPINSIGHTS_INSTRUMENTATIONKEY=e210bb4c-ee0f-4d1f-a892-9b9728fec526" -p 80:80/tcp --name CSDiscord --hostname CSDiscord --restart always "cisien/csdiscord:$tag"
+	docker run -d -e "tokens=$env:TOKENS" -e "APPINSIGHTS_INSTRUMENTATIONKEY=e210bb4c-ee0f-4d1f-a892-9b9728fec526" -p 80:80/tcp --name=CSDiscord --net=nat --net-alias=CSDiscord --hostname=CSDiscord --restart=always "cisien/csdiscord:$tag"
 }
