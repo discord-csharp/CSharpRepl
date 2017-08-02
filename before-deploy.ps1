eaff895e6870afa34c74e6eb7e47796687ab3600
Chris Curwick <ccurwick@exoronet.net>
2017-08-01 17:54:41 -0700

    Stop deploying containers for PRs set hostname of docker container

--- before-deploy.ps1
+++ before-deploy.ps1
@@ -1,17 +1,17 @@
-$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Continue';
 
-$tag = $env:APPVEYOR_REPO_BRANCH
 if(-not [System.String]::IsNullOrWhitespace($env:APPVEYOR_PULL_REQUEST_NUMBER)) {
-	$tag = "$tag-pr-${$env:APPVEYOR_PULL_REQUEST_NUMBER}"
+	return;
 }
 
+$tag = $env:APPVEYOR_REPO_BRANCH
 if([System.String]::IsNullOrWhitespace($tag)) {
     $tag = "untagged"
 }
 if (Enter-OncePerDeployment "install_docker_image")
 {
-docker stop CSDiscord
-docker rm CSDiscord
-docker pull "cisien/csdiscord:$tag"
-docker run -d -e "tokens=$env:TOKENS" -e "APPINSIGHTS_INSTRUMENTATIONKEY=e210bb4c-ee0f-4d1f-a892-9b9728fec526" -p 80:80/tcp --name CSDiscord "cisien/csdiscord:$tag"
+	docker stop CSDiscord
+	docker rm CSDiscord
+	docker pull "cisien/csdiscord:$tag"
+	docker run -d -e "tokens=$env:TOKENS" -e "APPINSIGHTS_INSTRUMENTATIONKEY=e210bb4c-ee0f-4d1f-a892-9b9728fec526" -p 80:80/tcp --name CSDiscord --hostname CSDiscord "cisien/csdiscord:$tag"
 }
