4c458ca8144484ba1f239e950e3ad350f0a37e07
Cisien <ccurwick@exoronet.net>
2018-08-09 19:45:53 -0700

    add dumb liveness probe url

---
+++ CSDiscordService/Controllers/StatusController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+
+namespace CSDiscordService.Controllers
+{
+    [Route("[controller]")]
+    public class StatusController : Controller
+    {
+        private ILogger<StatusController> _logger;
+
+        public StatusController(ILogger<StatusController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("probe")]
+        public async Task<ActionResult<string>> StatusProbeAsync()
+        {
+            await Task.CompletedTask;
+            return Ok("Ok");
+        }
+    }
+}
