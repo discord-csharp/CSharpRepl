9bc7fa46b6982d473aa600af3f05bceaf0039b8f
Scott Caldwell <sccaldwell92@gmail.com>
2019-12-30 14:57:53 -0500

    Add converter to serialize ValueTuples.
    Uses a factory, because we only want to match on ValueTuple (regular Tuple also implements ITuple).
    
    ValueTupleConverter implements JsonConverter<object> instead of JsonConverter<ITuple>, because I was getting an exception otherwise:
    "An exception occurred when serializing the response: ArgumentException: GenericArguments[1], 'System.Object', on 'System.Text.Json.Serialization.JsonPropertyInfoNotNullableContravariant`4[TClass,TDeclaredProperty,TRuntimeProperty,TConverter]' violates the constraint of type 'TDeclaredProperty'."

---
+++ CSDiscordService/Infrastructure/JsonFormatters/ValueTupleConverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace CSDiscordService.Infrastructure.JsonFormatters
+{
+    public class ValueTupleConverterFactory : JsonConverterFactory
+    {
+        public override bool CanConvert(Type typeToConvert)
+            => typeToConvert.Namespace == nameof(System)
+            && typeToConvert.IsValueType
+            && typeof(ITuple).IsAssignableFrom(typeToConvert);
+
+        public override JsonConverter CreateConverter(Type typeToConvert, JsonSerializerOptions options)
+            => new ValueTupleConverter();
+
+        private class ValueTupleConverter : JsonConverter<object>
+        {
+            public override object Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+                => throw new NotSupportedException();
+
+            public override void Write(Utf8JsonWriter writer, object value, JsonSerializerOptions options)
+            {
+                var tuple = (ITuple)value;
+
+                writer.WriteStartObject();
+
+                for (var i = 0; i < tuple.Length; i++)
+                {
+                    writer.WritePropertyName($"Item{i + 1}");
+                    JsonSerializer.Serialize(writer, tuple[i], options);
+                }
+
+                writer.WriteEndObject();
+            }
+        }
+    }
+}
